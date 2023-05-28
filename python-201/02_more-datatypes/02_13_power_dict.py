d5acf9951aec2d3bd50f314ae574f7453e571101
Daniel Ahau <daniel.ahau@gmail.com>
2023-05-28 15:41:56 +0530

    1.0

--- python-201/02_more-datatypes/02_13_power_dict.py
+++ python-201/02_more-datatypes/02_13_power_dict.py
@@ -1,3 +1,22 @@
 # Write a script that creates a dictionary of keys, `n`
 # and values `n * n` for numbers 1 to 10. For example:
 # result = {1: 1, 2: 4, 3: 9, ... and so on}
+
+dict_1 = {}
+
+keys = range(11)
+
+values = []
+
+a = None
+
+for i in keys:
+    a = i*i
+    values.append(a)
+
+
+for k in keys:
+    dict_1[k] = values[k]
+
+
+print(dict_1)
