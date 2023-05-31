1dca20fa1cdab9f458d8e655fd11a1907e3ccdbf
JayRam <daniel.ahau@gmail.com>
2023-05-31 22:58:19 +0530

    1.0

--- python-201/03_file-input-output/03_01_long_words.py
+++ python-201/03_file-input-output/03_01_long_words.py
@@ -1,3 +1,7 @@
 # Write a program that reads in `words.txt` and prints only the words
 # that have more than 20 characters (not counting whitespace).
 
+with open("words.txt", "r") as file_in:
+    for word in file_in:
+        if len(word) > 20:
+            print(word)
--- python-201/03_file-input-output/03_02_words_analysis.py
+++ python-201/03_file-input-output/03_02_words_analysis.py
@@ -4,3 +4,19 @@
 # 1. The shortest word (if there is a tie, print all)
 # 2. The longest word (if there is a tie, print all)
 # 3. The total number of words in the file.
+
+
+with open("words.txt", "r") as file:
+    words = file.read().split()
+
+shortest_length = len(min(words, key=len))
+shortes_words = [word for word in words if len(word) == shortest_length]
+
+longest_length = len(max(words, key=len))
+longest_word = [word for word in words if len(word) == longest_length]
+
+w_count = len(words)
+
+print("The shortest word(s) is/ are:", *shortes_words, sep="\n")
+print("The longest word(s) is/are: ", *longest_word, sep="\n" )
+print("The total word count is: ", w_count)
