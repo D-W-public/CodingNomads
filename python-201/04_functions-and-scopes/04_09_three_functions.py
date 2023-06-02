282016abb793d855357a2032f3363a01d3b8f84d
JayRam <daniel.ahau@gmail.com>
2023-06-02 22:57:06 +0530

    1.0

--- python-201/04_functions-and-scopes/04_09_three_functions.py
+++ python-201/04_functions-and-scopes/04_09_three_functions.py
@@ -3,3 +3,23 @@
 # of that function to do something with it. You can have more
 # than three functions, and they don't need to call each other
 # in a circular way.
+def years_to_days(years):
+    days = years/365
+    return days
+
+def years_to_hours(years):
+    days = years_to_days(years)
+    hours = days/24
+    return hours
+
+def years_to_minutes(years):
+    hours = years_to_hours(years)
+    minutes = hours/60
+    return minutes
+
+def years_to_seconds(years):
+    minutes = years_to_minutes(years)
+    seconds = minutes/60
+    return seconds
+
+print(years_to_seconds(1))
