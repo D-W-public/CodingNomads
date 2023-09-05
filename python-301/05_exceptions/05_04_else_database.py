748b45883d8e3257836c6fbe702b50884ea230b6
Jay_Ram <daniel.ahau@gmail.com>
2023-09-05 11:35:22 +0200

    clean up

--- python-301/05_exceptions/05_04_else_database.py
+++ python-301/05_exceptions/05_04_else_database.py
@@ -2,3 +2,6 @@
 # For example, you can revisit the course module about database interactions
 # and include a try/except/else statement based on what to do whether or not
 # the database connection can be established.
+
+#Done
+# py-101 guess the number game
