feb61e3f346aac010c3900677a4330e5a20f24b1
Jay_Ram <daniel.ahau@gmail.com>
2023-09-05 15:01:42 +0200

    3.0

--- python-301/projects/make-recipe-page/redditapi-get-token.py
+++ python-301/projects/make-recipe-page/redditapi-get-token.py
@@ -1,2 +1,25 @@
 # Demonstrate how you can log in to the Reddit API to receive content that
 # requires authentication, using only `requests` and your credentials.
+import requests
+from pprint import pprint
+import json
+import os
+import praw
+
+
+user_name = os.environ['reddit_username']
+reddit_password = os.environ['reddit_password']
+
+user_pass_dict = {
+     "user":user_name,
+     "passwd":reddit_password,
+     "api_type":"json"
+ }
+
+client = requests.session()
+
+r = client.post("https://www.reddit.com/api/login", data=user_pass_dict)
+
+j = json.loads(r.content)
+
+print(j)
