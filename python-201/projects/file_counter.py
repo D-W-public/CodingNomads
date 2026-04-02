f95f27687a5b741c407c31c2778706f4d8aacc52
Ishvar <ishvar108@proton.me>
2026-04-02 11:53:35 +0530

    done

--- python-201/projects/file_counter.py
+++ python-201/projects/file_counter.py
@@ -1,1 +1,19 @@
 # Add the code for the file counter script that you wrote in the course.
+
+from pathlib import Path
+
+down = Path.home() / "Downloads"
+
+filetypes = {}
+
+for item in down.iterdir():
+    if item.is_file():
+        ext = item.suffix
+
+        if ext in filetypes:
+            filetypes[ext] += 1
+
+        else:
+            filetypes[ext] = 1
+
+print(filetypes)
