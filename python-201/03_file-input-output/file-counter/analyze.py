f59f140ba98a391f007eee0b32099153473f278a
Ishvar <ishvar108@proton.me>
2026-04-04 21:09:24 +0530

    done

--- python-201/03_file-input-output/file-counter/analyze.py
+++ python-201/03_file-input-output/file-counter/analyze.py
@@ -1,1 +1,9 @@
 # Use the `csv` module to read in and count the different file types.
+
+import csv
+
+with open("filecounts.csv", "r") as csvfile:
+    reader = csv.DictReader(csvfile, fieldnames=["ISO", "ZIP", "MKV", "PNG", "JSON"])
+    counts = list(reader)
+
+print(counts)
--- python-201/03_file-input-output/file-counter/csvwrite.py
+++ python-201/03_file-input-output/file-counter/csvwrite.py
@@ -1,1 +1,35 @@
 # Write the file counts to a `.csv` file.
+import csv
+from pathlib import Path
+
+test = Path.home() / "Documents" / "test"
+output = (
+    Path.home()
+    / "CodingNomads"
+    / "python-201"
+    / "03_file-input-output"
+    / "file-counter"
+    / "filecounts.csv"
+)
+filetypes = {}
+
+for item in test.iterdir():
+    if item.is_file():
+        ext = item.suffix
+
+        if ext in filetypes:
+            filetypes[ext] += 1
+
+        else:
+            filetypes[ext] = 1
+
+with output.open("a") as csvfile:
+    countwriter = csv.writer(csvfile)
+    data = [
+        filetypes[".iso"],
+        filetypes[".zip"],
+        filetypes[".mkv"],
+        filetypes[".png"],
+        filetypes[".json"],
+    ]
+    countwriter.writerow(data)
